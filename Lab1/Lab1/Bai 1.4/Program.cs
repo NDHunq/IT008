ea93f7deb7b0c458fc3c77a69521d98ef1c110de
KKuyen <144935744+KKuyen@users.noreply.github.com>
2023-10-04 11:25:39 +0700

    bai1.4

---
+++ Lab1/Lab1/Bai 1.4/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1._4
+{
+    public class Bai_14
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao so lan nhiet ke thay doi nhiet do:");
+            int n=int.Parse(Console.ReadLine());
+            NhietKe nk =new NhietKe();
+            ManHinh mh =new ManHinh();
+            nk.set_random_Nhietdo();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Nhiet do hien thi tren man hinh lan thu {i + 1} la:");
+                nk.update_NhietDo();
+                mh.HienThi(nk);
+
+            }   
+                
+        }
+    }
+}
