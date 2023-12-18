5a425a5269b24b38ac09cb9463fede7b246d2481
huyhoang462 <22520462@gm.uit.edu.vn>
2023-12-18 15:38:27 +0700

    sua pham tim

--- Project/Instagram/Instagram/Tinh_nang/Tim.xaml.cs
+++ Project/Instagram/Instagram/Tinh_nang/Tim.xaml.cs
@@ -58,86 +58,93 @@ namespace Instagram.Tinh_nang
 
         private void btn_Copy1_Click(object sender, RoutedEventArgs e)
         {
-
-            bool isNum = int.TryParse(timNumber.Text, out int jj);
-            if (timAll.IsChecked == false && !isNum)
+            if (string.IsNullOrEmpty(username_tb.Text) && string.IsNullOrEmpty(Ds.Text))
+                MessageBox.Show("Vui lòng nhập đủ thông tin", "Thông báo");
+            else
             {
-
-                MessageBox.Show("Vui lòng chọn số lượng ảnh phù hợp");
-            }
-            soBaiTim = int.Parse(timNumber.Text);
-
-            List<string> ListUser = new List<string>();
-            if (username_tb.Text != "")
-            {
-                FileStream fs = new FileStream(username_tb.Text, FileMode.Open, FileAccess.Read);
-                StreamReader sr = new StreamReader(fs);
-                string l;
-                while ((l = sr.ReadLine()) != null)
+                bool isNum = int.TryParse(timNumber.Text, out soBaiTim);
+                if (rb_sl.IsChecked == true && !isNum)
                 {
-                    ListUser.Add(l);
+                    MessageBox.Show("Vui lòng chọn số lượng ảnh phù hợp");
+                    return;
                 }
-            }
-            string[] temp = new string[100];
-            temp = Ds.Text.Split(new string[] { "\n" }, StringSplitOptions.None);
-            foreach (string s in temp)
-            {
-                ListUser.Add(s);
-            }
-            if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(user_name))
-            {
-                if (ListUser.Count > 0)
+
+
+                List<string> ListUser = new List<string>();
+                if (!string.IsNullOrEmpty(username_tb.Text))
                 {
-                    driver = new ChromeDriver();
-                    try
+                    FileStream fs = new FileStream(username_tb.Text, FileMode.Open, FileAccess.Read);
+                    StreamReader sr = new StreamReader(fs);
+                    string l;
+                    while ((l = sr.ReadLine()) != null)
+                    {
+                        ListUser.Add(l);
+                    }
+                }
+                string[] temp = new string[100];
+                temp = Ds.Text.Split(new string[] { "\n" }, StringSplitOptions.None);
+                foreach (string s in temp)
+                {
+                    if (!string.IsNullOrEmpty(s))
+                        ListUser.Add(s);
+                }
+                if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(user_name))
+                {
+                    if (ListUser.Count > 0)
                     {
 
-
-                        driver.Url = "https://www.instagram.com/";
-                        driver.Navigate();
-                        System.Threading.Thread.Sleep(3000);
-
-                        IWebElement usernameInput = driver.FindElement(By.CssSelector("input[name='username']"));
-                        IWebElement passwordInput = driver.FindElement(By.CssSelector("input[name='password']"));
-                        usernameInput.SendKeys(user_name);
-                        passwordInput.SendKeys(password);
-
-
-                        // Click vào nút Đăng nhập
-                        IWebElement loginButton = driver.FindElement(By.CssSelector("button[type='submit']"));
-                        loginButton.Click();
-                        System.Threading.Thread.Sleep(5000);
-                        if (driver.Url == "https://www.instagram.com/")
+                        driver = new ChromeDriver();
+                        try
                         {
 
-                            throw new Exception("Đăng nhập thất bại!");
-                        }
 
-                        for (int i = 0; i < ListUser.Count; i++)
+                            driver.Url = "https://www.instagram.com/";
+                            driver.Navigate();
+                            System.Threading.Thread.Sleep(3000);
+
+                            IWebElement usernameInput = driver.FindElement(By.CssSelector("input[name='username']"));
+                            IWebElement passwordInput = driver.FindElement(By.CssSelector("input[name='password']"));
+                            usernameInput.SendKeys(user_name);
+                            passwordInput.SendKeys(password);
+
+
+                            // Click vào nút Đăng nhập
+                            IWebElement loginButton = driver.FindElement(By.CssSelector("button[type='submit']"));
+                            loginButton.Click();
+                            System.Threading.Thread.Sleep(5000);
+                            if (driver.Url == "https://www.instagram.com/")
+                            {
+
+                                throw new Exception("Đăng nhập thất bại!");
+                            }
+
+                            for (int i = 0; i < ListUser.Count; i++)
+                            {
+                                driver.ExecuteScript("window.open('');");
+                                ReadOnlyCollection<string> tabs = driver.WindowHandles;
+                                driver.SwitchTo().Window(tabs[tabs.Count - 1]);
+
+
+                                try
+                                {
+                                    Tym(ListUser[i]);
+                                }
+                                catch { }
+                            }
+
+                            driver.Quit();
+                            MessageBox.Show("Hoàn thành", "Thông báo");
+
+                        }
+                        catch (Exception ex)
                         {
-                            driver.ExecuteScript("window.open('');");
-                            ReadOnlyCollection<string> tabs = driver.WindowHandles;
-                            driver.SwitchTo().Window(tabs[tabs.Count - 1]);
-
-
-
-                            
-                                Tym(ListUser[i]);
-                           
+                            driver.Quit();
+                            MessageBox.Show(ex.Message, "Thông báo");
+                            return;
                         }
 
-                        driver.Quit();
-                        MessageBox.Show("Hoàn thành"+soBaiTim, "Thông báo");
 
                     }
-                    catch (Exception ex)
-                    {
-                        driver.Quit();
-                        MessageBox.Show(ex.Message, "Thông báo1");
-                        return;
-                    }
-
-
                 }
             }
         }
@@ -147,66 +154,73 @@ namespace Instagram.Tinh_nang
 
             int liked_post = 0;
 
-
-            string target_user = user.Substring(0, 26);
-            if (target_user == "https://www.instagram.com/")
+            try
             {
-                driver.Navigate().GoToUrl(user);
+                string target_user = user.Substring(0, 26);
+                if (target_user == "https://www.instagram.com/")
+                {
+                    driver.Navigate().GoToUrl(user);
+                }
+                else
+                    driver.Navigate().GoToUrl("https://www.instagram.com/" + user + "/");
             }
-            else
+            catch
+            {
                 driver.Navigate().GoToUrl("https://www.instagram.com/" + user + "/");
-
+            }
 
             System.Threading.Thread.Sleep(7000);
 
+
+
             try
             {
-
-                try
-                {
-                    IWebElement postSelector = driver.FindElement(By.CssSelector("div._aabd._aa8k._al3l a.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._a6hd"));
-                    postSelector.Click();
-                }
-                catch
-                {
-
-                }
-                while (true)
-                {
-
-                    ReadOnlyCollection<IWebElement> likeButtons = driver.FindElements(By.CssSelector("span._aamw svg[aria-label='Like']"));
-                    if (likeButtons.Count() > 0)
-                    {
-                        IWebElement likeButton = driver.FindElement(By.CssSelector("span._aamw svg[aria-label='Like']"));
-                        likeButton.Click();
-                        liked_post++;
-                    }
-                    By nextButtonSelector = By.CssSelector("svg[aria-label='Next']");
-                    // Kiểm tra sự tồn tại của nút "Next"
-                    IList<IWebElement> nextButtons = driver.FindElements(nextButtonSelector);
-                    if (nextButtons.Count != 0)
-                    {
-                        IWebElement nextButton = nextButtons[0];
-                        nextButton.Click();
-                    }
-                    else return;
-                    if (timAll.IsChecked == false && liked_post >= soBaiTim)
-                        return;
-                    System.Threading.Thread.Sleep(2000);
-
-                }
+                IWebElement postSelector = driver.FindElement(By.CssSelector("div._aabd._aa8k._al3l a.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._a6hd"));
+                postSelector.Click();
             }
-            catch { return; }
-        }
+            catch
+            {
+                return;
+            }
+            while (true)
+            {
 
-        private void timAll_Checked(object sender, RoutedEventArgs e)
-        {
-            timNumber.IsEnabled = false;
-        }
+                ReadOnlyCollection<IWebElement> likeButtons = driver.FindElements(By.CssSelector("span._aamw svg[aria-label='Like']"));
+                if (likeButtons.Count() > 0)
+                {
+                    IWebElement likeButton = driver.FindElement(By.CssSelector("span._aamw svg[aria-label='Like']"));
+                    likeButton.Click();
+                    System.Threading.Thread.Sleep(200);
+                    liked_post++;
+                }
+                By nextButtonSelector = By.CssSelector("svg[aria-label='Next']");
+                // Kiểm tra sự tồn tại của nút "Next"
+                IList<IWebElement> nextButtons = driver.FindElements(nextButtonSelector);
+                if (nextButtons.Count != 0)
+                {
+                    IWebElement nextButton = nextButtons[0];
+                    nextButton.Click();
+                }
+                else return;
+                if (rb_sl.IsChecked == true)
+                {
+                    if (liked_post >= soBaiTim)
+                        return;
+                }
+                System.Threading.Thread.Sleep(2000);
 
-        private void timAll_Unchecked(object sender, RoutedEventArgs e)
+            }
+        }
+        private void rb_sl_Checked(object sender, RoutedEventArgs e)
         {
             timNumber.IsEnabled = true;
+
+        }
+
+        private void rb_sl_Unchecked(object sender, RoutedEventArgs e)
+        {
+            timNumber.IsEnabled = false;
+            timNumber.Text = string.Empty;
         }
     }
 
